ed0efa9adb1fb5a63df19d3b253decfe5d89341a
Kritawat <gritawat@gmail.com>
2026-09-02 01:56:25 +0700

    confix email

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,9 @@ app.use(express.json());
 
 // ตั้งค่าตัวส่งอีเมล (Gmail Transporter)
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
